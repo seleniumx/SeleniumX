bae47fc346f7d2da3a699012dcbebb970706c3c7
Aravinda Weerasekara <aravinda.w@viewQwest.com>
2018-11-12 10:22:41 +0800

    Add @Page annotation feature

---
+++ src/main/java/org/seleniumx/annotations/Page.java
@@ -0,0 +1,13 @@
+package org.seleniumx.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({METHOD})
+public @interface Page {
+    Class[] page();
+}
